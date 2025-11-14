c7372cda5a73c17d6ec7f8eb9ca80e2807a2f6ea
EnzoCarrilho <enzocarrilho25@gmail.com>
2025-11-14 16:54:42 -0300

    adicionando funções de fetch a API

--- main.js
+++ main.js
@@ -17,4 +17,23 @@ async function getUserProfile(number){
     return data
 }
 
+async function getUserContacts(number){
+    const url = `https://api-whatsapp-9nb5.onrender.com/v1/whatsapp/contatos/${number}`
+    const response = await fetch(url)
+    const data = await response.json()
+    console.log(data)
+    return data
+}
+
+async function getUserMessages(userNumber, contactNumber){
+    const url = `https://api-whatsapp-9nb5.onrender.com/v1/whatsapp/${userNumber}?contactNumber=${contactNumber}`
+    const response = await fetch(url)
+    const data = await response.json()
+    console.log(data)
+    return data
+}
+
 listarTodosUsuarios()
+getUserProfile(11987876567)
+getUserContacts(11987876567)
+getUserMessages(11987876567, 2699999964)
